b6690db130a6f2be44e10756ff71c00b1324b6f0
abdou1919 <abdou1919@hotmail.com>
2015-04-21 14:17:18 +0200

    primo commit

---
+++ src/it/uniroma3/servelt/LeggiParametri.java
@@ -0,0 +1,9 @@
+package it.uniroma3.servelt;
+
+public class LeggiParametri {
+
+	public LeggiParametri() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/it/uniroma3/servelt/MostraParametri.java
@@ -0,0 +1,47 @@
+package it.uniroma3.servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/processaDati")
+public class MostraParametri extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		// gestione della RICHIESTA
+
+		// leggo i parametri
+		String nome = request.getParameter("nome").toUpperCase();
+		String cognome = request.getParameter("cognome").toUpperCase();
+
+		// leggo (alcune) intestazioni http della richiesta
+	
+
+		// gestione della RISPOSTA
+
+		// preparo il tipo (HTML)
+		response.setContentType("text/html");
+
+		// preparo un oggetto su cui scrivere la risposta
+		PrintWriter out = response.getWriter();
+
+		// scrivo il corpo
+		out.println("<!DOCTYPE html>"); out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"ISO-8859-1\" />");
+		out.println("<title>mostra parametri</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Dati inseriti nella form</h1>");
+		out.println("<ul>");
+		out.println("<li>Nome: <b>"+nome+"</b></li>");
+		out.println("<li>Cognome: <b>"+cognome+"</b></li>");
+		out.println("</body>\n</html> ");
+	}
+}
